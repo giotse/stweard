bd474d338b5110b2a19789a50d16383bddb4a4f4
giotse <giotse.code@gmail.com>
2019-04-17 21:03:05 -0300

    Initial version

---
+++ src/main/java/com/giotse/stweard/StweardApplication.java
@@ -0,0 +1,13 @@
+package com.giotse.stweard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StweardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StweardApplication.class, args);
+	}
+
+}
